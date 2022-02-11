3db707f53603b5d0a603d2c7d2cc1d7766b5adbd
Biniobiniasty <81110607+Biniobiniasty@users.noreply.github.com>
2022-02-11 10:34:15 -0800

    Update Useful function.cpp

--- Useful function.cpp
+++ Useful function.cpp
@@ -6,6 +6,8 @@ int read_digit() {
 	cin.clear();
 	cin.ignore();
 	cin.sync();
+	if (data.length() <= 0)
+		return 0;
 	for(int x=0;x<data.length();x++)
 		if (!((data[x] == '0') || (data[x] == '1') || (data[x] == '2') || (data[x] == '3') || (data[x] == '4') || (data[x] == '5')
 			|| (data[x] == '6') || (data[x] == '7') || (data[x] == '8') || (data[x] == '9')))
